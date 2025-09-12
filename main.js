233057f4fe6cdece2e4030a724d6450fedf8f3d9
Astrum-07 <astrumastro07@gmail.com>
2025-09-12 14:49:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// for (let i = 0;  true; i++) {
+//     console.log(i)  
+// }
+// let str = "engosnognogadadadaad";
+// let results=0
+// for (let i = 0; i < str.length; i++) {
+// if (str[i] === "a"){
+// results++
+// }    
+    
+// }
+
+
+
+// console.log(results)
+
+
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 10; j++) {
+      console.log(j,"j")
+        
+    }
+     console.log(i,"i")
+}

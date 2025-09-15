79ffc54c6db53ea055ea0a2afbc199abffdf4afb
Astrum-07 <astrumastro07@gmail.com>
2025-09-15 07:56:22 +0500

    salom

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 // for (let i = 0;  true; i++) {
 //     console.log(i)  
 // }
-// let str = "engosnognogadadadaad";
-// let results=0
-// for (let i = 0; i < str.length; i++) {
-// if (str[i] === "a"){
-// results++
-// }    
-    
+
+
+// for (let i = 2; i <= 140; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 101; i <= 239; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 42; i <= 180; i += 7){
+//     console.log(i);
+// }
+
+// for (let i = 55; i <= 440; i += 11){
+//     console.log(i);
+// }
+
+// for (let i = 100; i <= 210; i += 25){
+//     console.log(i);
+// }
+
+// for (let i = 25; i <= 690; i ++){
+//     if(i % 5 === 0 && i % 9 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 1; i <= 4000; i++){
+//     if(i % 11 === 0 && i % 3 === 0 && i % 9 === 0){
+//         console.log(i);
+//     }
 // }
 
 
 
-// console.log(results)
 
 
 
-for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 10; j++) {
-      console.log(j,"j")
-        
-    }
-     console.log(i,"i")
-}
+
